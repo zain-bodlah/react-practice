e51f08f1bdb91f7e7f9b4da60b6505710d9eb318
Zain Ahmad Bodla <zainbodla8@gmail.com>
2023-03-31 17:19:15 +0500

    react with typescript practice app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>submit</Button>
+    </div>
+  );
+}
+
+export default App;
